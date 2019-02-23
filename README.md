adb96630fa42fbb451331666a51612d2d5c5b440
Panji Sadewo <panji.8800@students.amikom.ac.id>
2019-02-23 17:25:40 +0700

    Employee Model,Controller,DAO

--- README.md
+++
@@ -1,2 +0,0 @@
-# MVC-HR
-Belajar menggunakan metode mvc pada database hr 
---
+++ src/models/Region.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Panji Sadewo
+ */
+public class Region {
+    private int id;
+    private String name;
+
+    public Region() {
+    }
+    
+    
+    public Region(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
