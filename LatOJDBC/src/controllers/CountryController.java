cbb28997edd037d03fc4dc64321918c36182b7c1
Gery Cahyandanu <gerydanu@live.com>
2019-02-25 08:37:20 +0700

    update CountryController

--- LatOJDBC/src/controllers/CountryController.java
+++ LatOJDBC/src/controllers/CountryController.java
@@ -5,10 +5,32 @@
  */
 package controllers;
 
+import daos.CountryDAO;
+import java.sql.Connection;
+import models.Country;
+
 /**
  *
  * @author gerydanu
  */
 public class CountryController {
+    
+    private CountryDAO cdao;
+    
+    public CountryController(Connection connection) {
+        cdao = new CountryDAO(connection);
+    }
+    
+    public String insert(String region_id, String region_name) {
+        String result = "";
+        if (cdao.save(new Country(country_id, country_name), true)) {
+            result = "YES! Data berhasil disimpan.";
+        }
+        else {
+            result = "OOPS! Data gagal disimpan.";
+        }
+        
+        return result;
+    }
     
 }
